0d11a989a3e95aa67e9ee5d9e9a18233968917a7
Daniel Sampson <sampsondg@outlook.com>
2025-04-03 13:36:05 -0300

    add register page - add button to nav to register - add styles to layout and index

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,23 @@
+import { COLORS } from "@/utils/colors";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={}/>
+    <Stack 
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: COLORS.background,
+      },
+      headerTintColor: '#fff',
+      contentStyle: {
+        backgroundColor: COLORS.background
+      }
+    }}>
+      <Stack.Screen name="index" options={{headerShown: false}}/>
+      <Stack.Screen name="register" options={{
+        title:'Create Account',
+        headerBackTitle:'Login', // for ios
+      }}/>
     </Stack>
 
   );
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,42 @@
-import { Text, View } from "react-native";
+import { COLORS } from "@/utils/colors";
+import { Link } from "expo-router";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <View style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href={'/register'} asChild>
+        <TouchableOpacity style={styles.outlineButton}>
+          <Text style={styles.outlineButtonText}>
+            Register!
+          </Text>
+        </TouchableOpacity>
+      </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'center',
+    backgroundColor: COLORS.background,
+  },
+  outlineButton: {
+    alignItems:'center',
+    padding: 12,
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: COLORS.primary,
+    marginVertical: 8,
+    backgroundColor: 'transparent',
+
+  },
+  outlineButtonText: {  
+    color:'#fff',
+    fontSize: 16,
+    fontWeight: 600,
+  },
+})
