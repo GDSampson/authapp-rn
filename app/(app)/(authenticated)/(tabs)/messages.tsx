ae3cede17c88f7ae9bcc398c4f4ab3aab7a76625
Daniel Sampson <sampsondg@outlook.com>
2025-04-04 11:16:29 -0300

    add ErrorState component - add to messages page - add flatlist to messages to render all messages

--- app/(app)/(authenticated)/(tabs)/messages.tsx
+++ app/(app)/(authenticated)/(tabs)/messages.tsx
@@ -1,11 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Fab from '@/components/fab/fab'
+import { useQuery } from '@tanstack/react-query'
+import { fetchMessages } from '@/utils/api'
+import { ErrorState } from '@/components/common/ErrorState'
 
 const Page = () => {
+  const {data: messages, isLoading, isError, refetch} = useQuery({
+    queryKey: ['messages'],
+    queryFn: () => fetchMessages(),
+  })
+
+  if (isError) {
+    return <ErrorState message='Failed to fetch messages' onRetry={refetch}/>
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Messages</Text>
+      <FlatList
+        data={messages?.data}
+        renderItem={({item}) => <Text>{item.content}</Text>}
+      />
       <Fab/>
     </View>
   )
