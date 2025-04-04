2c897f84f93018439967cb37e0c9af33afe9c76b
Daniel Sampson <sampsondg@outlook.com>
2025-04-04 10:08:24 -0300

    setup fab and styling for messages page

--- app/(app)/(authenticated)/(tabs)/messages.tsx
+++ app/(app)/(authenticated)/(tabs)/messages.tsx
@@ -1,14 +1,20 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Fab from '@/components/fab/fab'
 
 const Page = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Messages</Text>
+      <Fab/>
     </View>
   )
 }
 
 export default Page
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+  }
+})
