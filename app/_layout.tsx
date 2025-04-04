93976e5e1be407f3036bdecfecf7f01b41185902
Daniel Sampson <sampsondg@outlook.com>
2025-04-04 13:37:22 -0300

    add a google font to app - add useFonts to root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AuthProvider } from '@/context/AuthContext'
 import { Slot } from 'expo-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Inter_900Black, useFonts, Inter_400Regular } from '@expo-google-fonts/inter';
+import * as SplashScreen from 'expo-splash-screen';
+
+//keep the splash screen until I tell the app to hide it
+SplashScreen.preventAutoHideAsync();
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -12,6 +17,25 @@ const queryClient = new QueryClient({
 });
 
 const RootLayout = () => {
+  // load the custom fonts from the expo font library
+let [fonstLoaded] = useFonts({
+  Inter_900Black,
+  Inter_400Regular,
+})
+
+// hide the splash screen once fonts are loaded
+useEffect(() =>{
+  if (fonstLoaded) {
+    SplashScreen.hideAsync();
+  }  
+} ,[fonstLoaded])
+
+// prevent rendering the app until fonts are fully loaded
+// this avoids layout shifts or unstyled text flashing
+if (!fonstLoaded) {
+  return null;
+}
+
   return (
     <AuthProvider>
        <QueryClientProvider client={queryClient}>
