05de15256f143cbfdb52024f65937c382bbd3e29
Daniel Sampson <sampsondg@outlook.com>
2025-04-04 10:08:06 -0300

    tried to setup formSheet stack.screen - had to switch to modal as I couldn't get it to work

--- app/(app)/(authenticated)/_layout.tsx
+++ app/(app)/(authenticated)/_layout.tsx
@@ -1,11 +1,39 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
+import { COLORS } from '@/utils/colors'
 
 const Layout = () => {
   return (
-    <Stack>
-        <Stack.Screen name='(tabs)' options={{headerShown: false}}/>
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: COLORS.background,
+        },
+        headerTintColor: '#fff',
+        contentStyle: {
+          backgroundColor: COLORS.background
+        },
+        headerTitleStyle: {
+          color: '#fff',
+          fontWeight: 'bold',
+        },
+        
+      }}>
+      <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+      <Stack.Screen
+        name="new-msg"
+        options={{
+          presentation: 'modal',
+          title: 'New Message',
+          headerShown: true,
+          sheetAllowedDetents: [0.3, 0.8],
+          sheetGrabberVisible: true, // ios only
+          sheetExpandsWhenScrolledToEdge: false, // ios
+          sheetCornerRadius: 14,
+          
+        }}
+      />
     </Stack>
   )
 }
