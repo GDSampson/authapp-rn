66972fc3e68e353c282d1bc7f661fe85c21a87d1
Daniel Sampson <sampsondg@outlook.com>
2025-04-04 13:51:14 -0300

    add second file to render button differently on android

---
+++ components/fab/fab.android.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import { Link } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import { COLORS } from '@/utils/colors';
+
+export const FAB = () => {
+  return (
+    <Link href="/(app)/(authenticated)/new-msg" asChild>
+      <TouchableOpacity style={styles.fab}>
+        <Ionicons name="add" size={24} color="#fff" />
+      </TouchableOpacity>
+    </Link>
+  );
+};
+
+const styles = StyleSheet.create({
+  fab: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    backgroundColor: '#000',
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+    justifyContent: 'center',
+    alignItems: 'center',
+    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
+  },
+});
+
+export default FAB;
