0ee9aeda2db974ac4e18839d940fa71339521679
Daniel Sampson <sampsondg@outlook.com>
2025-04-04 10:06:59 -0300

    setup tanstack query client in top new rootlayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import { AuthProvider } from '@/context/AuthContext'
 import { Slot } from 'expo-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000, // 1 minute
+    },
+  },
+});
 
 const RootLayout = () => {
   return (
     <AuthProvider>
-        <Slot/>
+       <QueryClientProvider client={queryClient}>
+          <Slot />
+        </QueryClientProvider>
     </AuthProvider>
   )
 }
